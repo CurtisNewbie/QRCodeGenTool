b46a70d3a1ab5cf06ba450cc6c21228cf51978bc
CurtisNewbie <zhuangyongj@gmail.com>
2020-04-14 15:50:21 +0800

    Impl the func to parse arguments

--- my-app/src/main/java/com/curtisnewbie/app/App.java
+++ my-app/src/main/java/com/curtisnewbie/app/App.java
@@ -15,35 +15,37 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
  */
 public class App {
 
+    // arguments
+    public static final String MSG_ARG = "-m";
+    public static final String WIDTH_ARG = "-w";
+    public static final String HEIGHT_ARG = "-h";
+    public static final String OUTPUT_ARG = "-o";
+    public static final int ARG_LEN = 4;
+
     public static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        // get console input
-        System.out.println("\n------------------------"
-                + "\n\nThis is the QRCodeGenerator. Please enter the text that you want to encode:"
-                + "\n\n------------------------");
-        String text = in.nextLine();
-
-        System.out.println("\nPlease specify the width of the QR code image [whole number/int]");
-        int width = in.nextInt();
-
-        System.out.println("\nPlease specify the height of the QR code image [whole number/int]");
-        int height = in.nextInt();
-
-        if (width <= 0 || height <= 0) {
-            System.out.println("Width and height must be greater than 0");
-            System.exit(0);
+        var map = parseArgs(args);
+        if (map.size() != ARG_LEN) {
+            printArgsList();
+            System.exit(1);
         }
 
-        System.out.println("\nPlease specify the output path of the image");
-        in.nextLine();
-        String outputPath = in.nextLine();
-
-        System.out.println("GeneratingQRIMage:...");
         try {
+            int width = Integer.parseInt(map.get(Param.WIDTH));
+            int height = Integer.parseInt(map.get(Param.HEIGHT));
+            String msg = map.get(Param.MSG);
+            String outputPath = map.get(Param.OUTPUT_PATH);
+
+            if (width <= 0 || height <= 0) {
+                System.err.println("Width and height must be greater than 0");
+                System.exit(1);
+            }
+
+            System.out.println("Generating QR Image...");
             // generate QR code image
-            generateQRImage(text, width, height, outputPath);
+            generateQRImage(msg, width, height, outputPath);
             System.out.println("Done!");
 
             // open the created images
@@ -55,14 +57,15 @@ public class App {
             }
 
         } catch (WriterException e) {
-            System.out.println("Failed to generate QR image, existing...");
-            System.out.println("\n" + e.toString());
+            System.err.println("Failed to generate QR image, existing...");
+            System.err.println("\n" + e.toString());
             System.exit(0);
         } catch (IOException e) {
-            System.out.println("\n" + e.toString());
+            System.err.println("\n" + e.toString());
             System.exit(0);
+        } catch (NumberFormatException e) {
+            System.err.println("Width and Height must be Integer");
         }
-
     }
 
     /**
@@ -97,5 +100,52 @@ public class App {
 
         Runtime runtime = Runtime.getRuntime();
         runtime.exec("cmd.exe /c \"" + p + "\"");
+    }
+
+    /**
+     * Parse the arguments, and load them to the map
+     * 
+     * @param args
+     * @return {@code a Map<Parameters, String>} of parsed arguments
+     */
+    public static Map<Param, String> parseArgs(String args[]) {
+        Map<Param, String> argMap = new EnumMap<>(Param.class);
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals(MSG_ARG)) { // msg
+                if (!argMap.containsKey(Param.MSG)) {
+                    List<String> tempList = new ArrayList<>();
+                    while (i < args.length - 1) {
+                        var txt = args[++i];
+                        if (!txt.startsWith("-"))
+                            tempList.add(txt);
+                        else {
+                            --i;
+                            break;
+                        }
+                    }
+                    argMap.put(Param.MSG, String.join(" ", tempList));
+                }
+            } else if (args[i].equals(WIDTH_ARG)) { // width
+                if (!argMap.containsKey(Param.WIDTH) && i < args.length - 1)
+                    argMap.put(Param.WIDTH, args[++i]);
+            } else if (args[i].equals(HEIGHT_ARG)) { // height
+                if (!argMap.containsKey(Param.HEIGHT) && i < args.length - 1)
+                    argMap.put(Param.HEIGHT, args[++i]);
+            } else if (args[i].equals(OUTPUT_ARG)) { // output path
+                if (!argMap.containsKey(Param.OUTPUT_PATH) && i < args.length - 1)
+                    argMap.put(Param.OUTPUT_PATH, args[++i]);
+            }
+        }
+        return argMap;
+    }
+
+    public static void printArgsList() {
+        System.err.println(String.format(
+                "\n\u001B[1m\u001B[31m  Error - incorrect number of arguments.\u001B[39m\n  Arguments List:\n\t%s message to be encoded\n\t%s width of the QRcode (Integer)\n\t%s height of the QRcode (Integer)\n\t%s absolute/relative path of the output QRcode image\u001B[0m\n",
+                MSG_ARG, WIDTH_ARG, HEIGHT_ARG, OUTPUT_ARG));
+    }
+
+    enum Param {
+        MSG, WIDTH, HEIGHT, OUTPUT_PATH
     }
 }
